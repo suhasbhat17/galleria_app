cd93f768295406be7aa0b0473bba2da047d1fda7
Suhas <sbhat1@hawk.iit.edu>
2018-12-22 12:19:42 -0600

    Initial Commit

---
+++ src/main/java/com/suhas/vykhari/galleria/GalleriaApplication.java
@@ -0,0 +1,14 @@
+package com.suhas.vykhari.galleria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalleriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GalleriaApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/suhas/vykhari/galleria/GalleriaApplicationTests.java
@@ -0,0 +1,17 @@
+package com.suhas.vykhari.galleria;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GalleriaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
